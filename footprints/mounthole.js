2483d4aeabe39fab28c22e18b4d01f8de2ae23fe
Rowan Clarke <r.n.clarke@lancaster.ac.uk>
2024-06-22 18:02:22 +0100

    Custom Mountholes

--- footprints/mounthole.js
+++ footprints/mounthole.js
@@ -1,11 +1,14 @@
 
 module.exports = {
   params: {
-    designator: 'M2',
+    designator: 'countersunk',
+    thread: 0,
+    head: 0
   },
   body: p => {
+    let clearance = (p.head - p.thread) / 2 + 0.25
     return `
-      (module M2CS (layer F.Cu) (tedit 5DD50112)
+      (module CS (layer F.Cu) (tedit 5DD50112)
       ${p.at /* parametric position */}
 
       ${'' /* footprint reference */}
@@ -13,10 +16,10 @@ module.exports = {
       (fp_text value "" (at 0 0) (layer F.SilkS) hide (effects (font (size 1.27 1.27) (thickness 0.15))))
 
       ${''/* illustration of the countersunk */}
-      (fp_circle (center 0 0) (end 0 1.925) (layer Dwgs.User) (width 0.15))
+      (fp_circle (center 0 0) (end 0 ${p.head / 2}) (layer Dwgs.User) (width 0.15))
       
       ${'' /* stabilizers */}
-      (pad "" np_thru_hole circle (at 0 0) (size 2.2 2.2) (drill 2.2) (layers *.Cu *.Mask) (clearance 1.075)))
-    `
+      (pad "" np_thru_hole circle (at 0 0) (size ${p.thread} ${p.thread}) (drill ${p.thread}) (layers *.Cu *.Mask) (clearance ${clearance})))
+      `
   }
 } 
